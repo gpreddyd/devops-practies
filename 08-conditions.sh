3a14b91989867f3d74ef8a36ff5cf5c8e167b5e6
GopiREDDY <gdilaalapuram@gmail.com>
2024-08-27 09:38:21 +1000

    changes

---
+++ 08-conditions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#it shows how conditions are working in shell
+
+today=monday
+
+if[today="monday"]
+then
+
+echo "today we have class"
+
+exit 1
+
+echo "we can search for other days"
+
+else
+
+echo "we dont have class"
+ exit 0
+
+ echo " we can execyte exit statement"
+ 
+
+fi
