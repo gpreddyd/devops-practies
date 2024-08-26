a39a8a1e04c9b39752406ec04f3be4a79907d6d3
GopiREDDY <gdilaalapuram@gmail.com>
2024-08-26 20:11:16 +1000

    changes to convesation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh : Hi suresh, how are you? "
+echo "Suresh : hello Ramesh, i am goog , Thank you "
+echo "Ramesh :"whar are you doing"
+echo "Suresh : " i am learning Devops "
