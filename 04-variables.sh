1af83b6e74ace47ff84c1193fc6889cdf25730f3
GopiREDDY <gdilaalapuram@gmail.com>
2024-08-26 20:51:32 +1000

    chnages to var

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter you name :: "
+read USERNAME
+
+echo "the Entered User name is : $USERNAME "
+
+echo " Please Enter Password :: "
+ read  -s PASSWORD 
+
+ echo "password is : $PASSWORD "
