fad132e1365698db8664846f710ebed0253e5ea6
GopiREDDY <gdilaalapuram@gmail.com>
2024-08-27 20:28:46 +1000

    changes

---
+++ 10-functions.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+#echo "the user id is: $userid "
+
+validate(){
+
+    echo " The Exit Status is : $1 "
+
+}
+
+if [ $userid -ne 0 ]
+then
+    echo " Get the Root user Privilages "
+
+    exit 1
+fi
+
+dnf install git -y 
+
+validate $?
+
+if [ $? -ne 0 ]
+then
+    echo " git is not  installed, goint to install now... "
+    dnf install gitt -y 
+    if [ $? -ne 0 ]
+    then 
+        echo "git instation failure,please check "
+    fi
+else
+     echo " Git instalation is successsssss..."
+
+fi
+
+dnf install mysql -y 
+
+if [ $? -ne 0 ]
+then
+    echo " mysql is not  installed, goint to install now... "
+    dnf install mysql -y 
+    if [ $? -ne 0 ]
+    then 
+        echo "mysql instation failure,please check "
+    fi
+else
+     echo " mysql instalation is successsssss..."
+
+fi
