8a8d32f62f829de956e043a86181e9eb009b86f7
GopiREDDY <gdilaalapuram@gmail.com>
2024-08-27 10:52:12 +1000

    changes

--- 09-instaltions.sh
+++ 09-instaltions.sh
@@ -8,6 +8,17 @@ if [ $userid -ne 0 ]
 then
     echo " Get the Root user Privilages "
 
+    exit 1
 fi
 
 dnf install git -y 
+
+if [ $? -ne 0 ]
+then
+    echo " git is not  installed, goint to install now... "
+    dnf install git -y 
+
+else
+     echo " Git instalation is success..."
+
+fi
