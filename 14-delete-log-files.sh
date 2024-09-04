f86d6b8ef98e995b47dd045f0daf3ca04cde17f8
GopiREDDY <gdilaalapuram@gmail.com>
2024-09-04 20:40:05 +1000

    changes

---
+++ 14-delete-log-files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source_dir="/home/ec2-user/logs"
+ 
+ if [ -d $source_dir ]
+ then 
+     echo " source direcoty exists $source_dir "
+ else
+    echo " Source directory does not exists "
+    exit 1
+  
+  fi
+
+files=$(find $source_dir -name "*.log" -mtime +14)
+
+echo " files :$files "
