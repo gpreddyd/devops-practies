220ff0b8707e16dedde6843b4aa1274685650f92
GopiREDDY <gdilaalapuram@gmail.com>
2024-09-04 19:52:57 +1000

    changes

--- 13-set.sh
+++ 13-set.sh
@@ -2,6 +2,15 @@
 
 
 set -e 
+
+
+trap 'failure "$LINENO" "$BASH_COMMAND" ' ERR 
+
+failure(){
+
+    echo " failed at $1: $2 "
+}
+
 echo " the comand is success"
 echoo " it is filure"
 echo " it again success "
