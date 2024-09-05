9642fb7a891817a5d06067972dce82738bf9ea7a
GopiREDDY <gdilaalapuram@gmail.com>
2024-09-05 20:58:31 +1000

    changes

--- 15-backupfiles.sh
+++ 15-backupfiles.sh
@@ -3,6 +3,7 @@
 sdir=$1
 ddir=$2
 days=${3:-14} #if $3 is empty,default will be 14 days
+tstamp=$(date +%Y-%m-%d-%H-%M-%S)
 
 R="\e[31m"
 G="\e[32m"
@@ -36,9 +37,17 @@ files=$(find $sdir -name "*.log" -mtime +14)
 
 echo "files : $files "
 
+zip $
+
 if [ ! -z $files ]
 then
     echo "files are found"
+    
+    zfile= " $ddir/app-log.tstamp.zip "
+
+    find $sdir  -name "*.log" -mtime +14 | zip "$zfile" -@
+    
+
  else
     echo " files are not found older than 14 days "
 
