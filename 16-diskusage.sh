9a0926eb1aa3ea1f417707143244fc805d1ad2aa
GopiREDDY <gdilaalapuram@gmail.com>
2024-09-06 19:32:43 +1000

    changes

--- 16-diskusage.sh
+++ 16-diskusage.sh
@@ -6,6 +6,10 @@ diskthr=5
 while IFS= read -r line
 do
     usage=$(echo $line | grep xfs | awk -F " " '{print $6f}' | awk -F "%" '{print $1f}')
+    partition=$(echo $line | grep xfs | awk -F " " '{print $Nf}')
+
+    echo " $usage"
+    echo "$partition"
 
      if [[ $usage -gt $diskthr ]]
      then
