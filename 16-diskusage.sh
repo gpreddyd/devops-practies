a6f921b4148ec316e8aade2574a0eb0f90e8f244
GopiREDDY <gdilaalapuram@gmail.com>
2024-09-06 19:20:41 +1000

    changes

--- 16-diskusage.sh
+++ 16-diskusage.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 diskusage=$(df -hT | grep xfs)
-diskthr= 5
+diskthr=5
 
 while IFS= read -r line
 do
+
+
      if [[ $diskusage -gt $diskthr ]]
      then
      echo "the line is: $line "
